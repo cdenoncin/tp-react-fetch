5db84f011c8ac97e007954ef175a6f9329c9ac31
Cécile Denoncin <ce.denoncin@gmail.com>
2022-05-31 10:53:10 +0200

    tailwind added & sign in component WIP

---
+++ src/SignIn.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function SignIn() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Blablablablablabla sign in form
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default SignIn;
