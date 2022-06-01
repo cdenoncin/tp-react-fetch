a5e24984ccd16e9b59f89ea328a6f90378ee652b
timothee-durand <timothee.durand70@gmail.com>
2022-06-01 20:08:45 +0200

    add  api

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -6,4 +6,11 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ api/src/Controller/PostController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class PostController extends AbstractController
+{
+    #[Route('/api/posts', name: 'app_post', methods: ["GET"])]
+    public function index(PostRepository $repository, SerializerInterface $serializer): JsonResponse
+    {
+        return $this->json($serializer->serialize($repository->findAll(), 'json', ['groups' => ['posts']]));
+    }
+
+    #[Route('/api/posts', name: 'app_post', methods: ["POST"])]
+    public function create(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $manager,
+        TokenStorageInterface $tokenStorageInterface,
+        JWTTokenManagerInterface $jwtManager,
+        UserRepository $userRepository,
+    ): JsonResponse
+    {
+        $jwt = $jwtManager->decode($tokenStorageInterface->getToken());
+        $post = $serializer->deserialize($request->getContent(), Post::class, 'json');
+        $post->setAuthor($userRepository->findOneByEmail($jwt["email"]));
+        $manager->persist($post);
+        $manager->flush();
+        return $this->json($serializer->serialize($post, 'json', ['groups' => ['posts']]));
+    }
+
+    #[Route('/api/posts/{id}', name: 'app_post_show', methods: ["GET"])]
+    public function show(Post $post, SerializerInterface $serializer): JsonResponse
+    {
+        return $this->json($serializer->serialize($post, 'json', ['groups' => ['posts']]));
+    }
+
+    #[Route('/api/posts/{id}', name: 'app_post_delete', methods: ["DELETE"])]
+    public function delete(Post $post, SerializerInterface $serializer, EntityManagerInterface $manager): JsonResponse
+    {
+        $manager->remove($post);
+        $manager->flush();
+        return $this->json([
+            "message" => sprintf("Post %s was successfully deleted", $post->getId())
+        ]);
+    }
+}
